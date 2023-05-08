578d468ccce2b12a4dadf620661b0a46d176901f
Mário Cao <kreativermario@gmail.com>
2023-05-08 22:52:17 +0100

    added some functions to hyperparametertuning
    Signed-off-by: Mário Cao <kreativermario@gmail.com>

--- src/main/java/main/HyperparameterTuning.java
+++ src/main/java/main/HyperparameterTuning.java
@@ -55,36 +55,39 @@ public class HyperparameterTuning {
     }
 
     private void runSingle() {
-        int numRuns = 10; // number of runs with different settings
         Random random = new Random();
-        for (int i = 0; i < numRuns; i++) {
-            // Randomize hyperparameters
-            int populationSize = random.nextInt(900) + 100;
-            int nrFitIndividuals = random.nextInt(populationSize / 2) + 1;
-            double mutationProb = random.nextDouble();
-            int nrGenerations = random.nextInt(100) + 1;
-            int tournamentSize = random.nextInt(nrFitIndividuals) + 1;
-            int hiddenDimSize = random.nextInt(50) + 1;
-            int seed = random.nextInt(1000);
+        // Randomize hyperparameters
+        int populationSize = random.nextInt(900) + 100;
+        int nrFitIndividuals = random.nextInt(populationSize / 2) + 1;
+        double mutationProb = random.nextDouble();
+        int nrGenerations = random.nextInt(100) + 1;
+        int tournamentSize = random.nextInt(nrFitIndividuals) + 1;
+        int hiddenDimSize = random.nextInt(50) + 1;
+        int seed = random.nextInt(1000);
 
-            Hyperparameters hyperparameters = new Hyperparameters();
-            hyperparameters.setNrGenerations(nrGenerations);
-            hyperparameters.setNrFitIndividuals(nrFitIndividuals);
-            hyperparameters.setTournamentSize(tournamentSize);
-            hyperparameters.setSeed(seed);
-            hyperparameters.setHiddenDimSize(hiddenDimSize);
-            hyperparameters.setMutationProb(mutationProb);
-            hyperparameters.setPopulationSize(populationSize);
+        Hyperparameters hyperparameters = new Hyperparameters();
+        hyperparameters.setNrGenerations(nrGenerations);
+        hyperparameters.setNrFitIndividuals(nrFitIndividuals);
+        hyperparameters.setTournamentSize(tournamentSize);
+        hyperparameters.setSeed(seed);
+        hyperparameters.setHiddenDimSize(hiddenDimSize);
+        hyperparameters.setMutationProb(mutationProb);
+        hyperparameters.setPopulationSize(populationSize);
 
-            PopulationEvo populationEvo = new PopulationEvo(hyperparameters);
-            populationEvo.start();
-        }
+        PopulationEvo populationEvo = new PopulationEvo(hyperparameters);
+        populationEvo.start();
+
 
     }
 
 
     public static void main(String[] args) {
         HyperparameterTuning hyperparameterTuning = new HyperparameterTuning();
+//        int numRuns = 10; // number of runs with different settings
+//        Random random = new Random();
+//        for (int i = 0; i < numRuns; i++) {
+//            hyperparameterTuning.runSingle();
+//        }
         hyperparameterTuning.runSingle();
         //hyperparameterTuning.runMultiple();
     }
